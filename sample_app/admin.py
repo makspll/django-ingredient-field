45eddc5df3c8e3acdd0a16a2943704d556bd7478
makspll <makspl17@gmail.com>
2021-08-26 00:34:41 +0100

    removed sample_app

--- sample_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from sample_app.models import TestModel
-
-# Register your models here.
-
-@admin.register(TestModel)
-class AuthorAdmin(admin.ModelAdmin):
-    pass
--- sample_app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models 
-from dj_ingredient_field.fields import IngredientField
-
-class TestModel(models.Model):
-    ingredient = IngredientField()
