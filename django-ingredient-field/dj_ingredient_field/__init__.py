a369198c9028c4fc8c840eacd5e5a482b07d2244
makspll <makspl17@gmail.com>
2021-08-26 00:34:59 +0100

    moved Ingredient

--- django-ingredient-field/dj_ingredient_field/__init__.py
+++ django-ingredient-field/dj_ingredient_field/__init__.py
@@ -1,4 +1,16 @@
 __version__ = "0.2.7"
 
-from .fields import *
-from .enums import *
+from .fields import IngredientField
+from .enums import IngredientName
+
+class Ingredient():
+    def __init__(self, name : IngredientName ) -> None:
+        self.name = str(name)
+        print(self)
+
+    def __str__(self):
+        return str(self.name)
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, Ingredient) and o.name == self.name
+
--- django-ingredient-field/dj_ingredient_field/ingredient.py
+++
@@ -1,8 +0,0 @@
-from dj_ingredient_field.enums import IngredientNameChoices
-
-class Ingredient():
-
-    def __init__(self, ingredient_name : IngredientNameChoices ) -> None:
-        self.name = str(ingredient_name)
-        
-    
